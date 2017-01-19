c4210a8e88bc62821181537a43f3c1a4d4ee1b6b
meceq <zhuang25@illinois.edu>
2017-01-18 22:22:08 -0800

    add class Node

---
+++ Node.java
@@ -0,0 +1,9 @@
+public Node {
+    int val;
+    Node left, right;
+    public Node(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
--- Test.java
+++ Test.java
@@ -1,3 +1,4 @@
+// simple print
 class Test{
 	static public void main(String[] args) {
 		System.out.println("Testing");
